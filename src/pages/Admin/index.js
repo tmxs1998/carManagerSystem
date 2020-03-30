988a49851dcfb1c139d6fde4ddf5bcd1b54a2dd3
hypppp <781098538@qq.com>
2020-03-30 16:20:48 +0800

    init

---
+++ src/pages/Admin/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class Admin extends Component {
+  render() { 
+    return ( 
+      <div>这里是admin</div>
+     );
+  }
+}
+ 
+export default Admin;
---
+++ src/pages/Login/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class Login extends Component {
+  render() { 
+    return ( 
+      <div>这里是login</div>
+     );
+  }
+}
+ 
+export default Login;
